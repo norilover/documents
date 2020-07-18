a8c8f5899632ae7cb1c6ca7c93aa0d2bc7d03e36
norilover <hhhwzc_17@126.com>
2020-07-18 21:14:25 +0800

    JavaBasicData

---
+++ JavaData/javaBasicData.md
@@ -0,0 +1,18 @@
+# Java Basic Knowledge
+
+* <E super T>
+
+* <E extends T>
+
+  ```
+  <E super T> 
+  	向下限定：E是T的基类（父类）
+  		经常往里插入
+  		
+  <E extends T>
+  	想上限定：E是T的派生类（子类）
+  		频繁往外读取内容
+  ```
+
+  
+
--- Temp/Test.java
+++ Temp/Test.java
@@ -1,16 +1,42 @@
 public class Test{
 	public static void main(String[] args) {
-		String quifiedName = "Class";
-
-		char[] chars = quifiedName.toCharArray();
-		chars[0] += 32;
-
-		System.out.println(String.valueOf(chars));
+		List<String> ls = new ArrayList<>();
 
 
-		// 
-		String url = "www//nori//edu///cn";
+	}
+}
 
-		System.out.println(url.replaceAll("/+", "."));
+class Test1{
+
+	public List<?> getList(){
+
+	}
+
+	public Number getNumber( List<E extends Number> ln){
+
+	}
+
+	public T getT(List<T extends Comparable<T>){
+
+	}
+
+	public Object getObject(List<? extends Number>){
+
+	}
+
+	static <E> List<E> asList(E[] a){
+
+	}
+
+	public static <E> Set<E> union(Set<E> s1, Set<E> s2){
+
+	}
+
+	public Set<T> union(int a, int  b){
+
+	}
+
+	public static <E extends Comparable<E>> E max(Collection<E> c){
+		
 	}
 }
