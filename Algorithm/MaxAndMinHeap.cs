c71f6ff773e40c6d1cd58791ba1bb712f8898845
emereld <947122740@qq.com>
2021-05-27 11:17:24 +0800

    Al

---
+++ Algorithm/MaxAndMinHeap.cs
@@ -0,0 +1,24 @@
+﻿namespace DefaultNamespace
+{
+    public class MaxAndMinHeap
+    {
+        
+    }
+
+    public interface IHeap
+    {
+        int getParent(int i);
+        int getLeft(int i);
+        int getRight(int i);
+        int extractMin();
+        void decreaseKey(int i, int newValue);
+        int getMin();
+        void deleteKey(int i);
+        void insertKey(int i);
+    }
+
+    public class MinHeap : IHEap
+    {
+        
+    }
+}
